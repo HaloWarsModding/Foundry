80fc09cb62fbd688081d5dc8b300c410bb5cc0eb
jake1029 <55038561+jake1029@users.noreply.github.com>
2024-05-22 17:05:47 -0400

    Probably gonna need the entry point huh...

---
+++ foundry/Program.cs
@@ -0,0 +1,22 @@
+﻿using Foundry;
+using Foundry.HW1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foundry
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            WorkspaceUIWinforms window = new WorkspaceUIWinforms();
+            Application.Run(window);
+        }
+    }
+}
